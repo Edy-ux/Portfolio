2b794d2c8755ce8925878be7d01c5af830c0e8d3
fenderlopes@gmail.com <fenderlopes@gmail.com>
2021-10-31 20:34:38 -0300

    fist commit

---
+++ README.md
@@ -0,0 +1,46 @@
+<h1 align="center">
+  📰<br>Meu Portfólio
+</h1>
+
+<h4 align="center">
+  Portfólio criado com o intuito de mostrar meus projetos ao decorrer da minha carreira.
+</h4>
+
+![Resultado final do projeto](assets/image/preview.png)
+
+<h4 align="center"><a href="https://www.iuricode.com/">Clique para visitar o projeto</a></h4>
+
+## 📚 Seções
+O site é composto por 5 seções diferentes:
+
+- **Home:** Nele temos um avatar do meu github e um breve apresentação;
+- **Quem sou:** Nessa seção tenho uma descrição dizendo um pouco sobre quem sou;
+- **Projetos:** Apresenta alguns projetos desenvolvidos e com link direto para os respectivos códigos no GitHub;
+- **Serviços:** Exibe através de cards os diferentes serviços em que possuo conhecimentos;
+- **Conhecimentos:** Nele apresentamos meus conhecimentos em algumas linguagens como o foco no front-end;
+
+---
+
+## 💼 Tecnologias utilizadas
+Para o desenvolvimento deste site utilizei as seguintes tecnologias:
+
+- HTML;
+- Sass;
+- JavaScript;
+- ScrollReveal;
+
+---
+
+## 🦄 Autor<br>
+<table>
+  <tr>
+    <td align="center">
+      <a href="https://github.com/iuricode">
+        <img src="https://avatars3.githubusercontent.com/u/31936044" width="100px;" alt="Foto do Iuri Silva no GitHub"/><br>
+        <sub>
+          <b>Iuri Silva</b>
+        </sub>
+      </a>
+    </td>
+  </tr>
+</table>
---
+++ assets/js/main.js
@@ -0,0 +1,164 @@
+//Menu
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li"
+);
+mobileNavbar.init();
+
+//Maquina de escrever
+function typeWrite(elemento) {
+  const textoArray = elemento.innerHTML.split("");
+  elemento.innerHTML = " ";
+  textoArray.forEach(function (letra, i) {
+    setTimeout(function () {
+      elemento.innerHTML += letra;
+    }, 100 * i);
+  });
+}
+const titulo = document.querySelector(".maquina-escrever");
+typeWrite(titulo);
+
+//Revelar animação
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "50px",
+  duration: 2000,
+});
+
+//Home delay
+sr.reveal(".button", { delay: 200 });
+sr.reveal(".linguagensProjeto", { delay: 400 });
+sr.reveal(".imagem-espaco-delay", { delay: 200 });
+sr.reveal(".imagem-iuri-delay", { delay: 600 });
+sr.reveal(".scroll-delay", { delay: 1000 });
+//Sobre mim delay
+sr.reveal(".imagem-delay", {});
+sr.reveal(".titulo-delay", { delay: 200 });
+sr.reveal(".descricao-delay", { delay: 400 });
+sr.reveal(".cards-interval", { interval: 400 });
+
+const text = {
+  html: "HTML é uma linguagem baseada em marcação, onde marcamos os elementos para definir quais informações a página vai exibir. <br><br> 1 ano de experiência",
+  css: "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas HTML. <br><br> 1 ano de experiência",
+  js:"JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. <br><br> 1 ano de experiência",
+  sass:"Sass (o pré-processador) é uma linguagem de folhas de estilo. É uma simples linguagem de script usada em arquivos Sass. <br><br> 9 meses de experiência",
+  react: "React é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web. <br><br> 4 meses de experiência",
+  next: "Next.js é uma estrutura da web que permite funcionalidades como renderização do lado do servidor e geração de sites estáticos para web baseados em React. <br><br> 4 meses de experiência"
+
+}
+//Refatorando codigo abaixo 
+const knowning = document.querySelectorAll('#cards-conhecimentos article')
+
+function changeArticleText() {
+  knowning.forEach(function(el){
+    el.addEventListener("mouseover", function () {
+      //console.log("oi")
+      (el.classList.contains('html')) ? text1.innerHTML = text.html 
+      :(el.classList.contains('css')) ? text1.innerHTML = text.css 
+      :(el.classList.contains('js'))  ? text1.innerHTML = text.js 
+      :(el.classList.contains('sass'))? text1.innerHTML = text.sass 
+      :(el.classList.contains('react')) ? text1.innerHTML = text.react 
+      :(el.classList.contains('next')) ? text1.innerHTML = text.next 
+      :(el.classList.contains('styled')) ? text1.innerHTML = `Em Processo..`
+      :(el.classList.contains('tailwind')) ? text1.innerHTML = `Em Processo..`
+      : ''
+  })
+ })
+}
+
+changeArticleText()
+
+//Animação conhecimentos
+var leftBox = document.querySelectorAll(".sairCaixa");
+var text1 = document.querySelector(".mudarTextoDescricao");
+var leftBox = document.querySelectorAll(".sairCaixa");
+
+for (let element of leftBox) {
+  element.addEventListener("mouseout", ()=>  text1.innerHTML = `*passe o mouse no card para ler*`)
+}  
+
+
+/* document.querySelector(".html").addEventListener("mouseover", function () {
+  texto1.innerHTML =
+    "HTML é uma linguagem baseada em marcação, onde marcamos os elementos para definir quais informações a página vai exibir. <br><br> 1 ano de experiência";
+});
+
+document.querySelector(".css").addEventListener("mouseover", function () {
+  texto1.innerHTML =
+    "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas HTML. <br><br> 1 ano de experiência";
+});
+
+document.querySelector(".js").addEventListener("mouseover", function () {
+  texto1.innerHTML =
+    "JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. <br><br> 1 ano de experiência";
+});
+
+document.querySelector(".tailwind").addEventListener("mouseover", function () {
+  texto1.innerHTML =
+    "Tailwind CSS é um framework desenvolvido para maximizar o potencial do bom e velho CSS e levá-lo ainda mais longe. <br><br> 2 meses de experiência";
+});
+
+document.querySelector(".sass").addEventListener("mouseover", function () {
+  texto1.innerHTML =
+    "Sass (o pré-processador) é uma linguagem de folhas de estilo. É uma simples linguagem de script usada em arquivos Sass. <br><br> 9 meses de experiência";
+});
+
+document.querySelector(".react").addEventListener("mouseover", function () {
+  texto1.innerHTML =
+    "React é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web. <br><br> 4 meses de experiência";
+});
+
+document.querySelector(".next").addEventListener("mouseover", function () {
+  texto1.innerHTML =
+    "Next.js é uma estrutura da web que permite funcionalidades como renderização do lado do servidor e geração de sites estáticos para web baseados em React. <br><br> 4 meses de experiência";
+});
+
+document
+  .querySelector(".styled")
+  .addEventListener("mouseover", function mudarTexto8() {
+    texto1.innerHTML =
+      "Styled Components é uma biblioteca que utiliza o conceito de CSS-in-JS, ou seja, que nos permite escrever códigos CSS dentro do Javascript. <br><br> 4 meses de experiência";
+  }); */
+
+
+/* for (i = 0; i < sairCaixa.length; i++) {
+  sairCaixa[i].addEventListSener("mouseout", function mudarTextoNormal() {
+    text1.innerHTML = `*passe o mouse no card para ler*`;
+  });
+}  */
